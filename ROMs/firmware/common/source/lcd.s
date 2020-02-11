8f95e289135e01f8bf65da57a313fd926879ad1f
Dawid Buchwald <dawid.buchwald@hotmail.com>
2020-02-11 11:07:51 +0100

    Added single char output routine

--- ROMs/firmware/common/source/lcd.s
+++ ROMs/firmware/common/source/lcd.s
@@ -3,6 +3,7 @@
       .include "utils.inc"
       .export _lcd_init
       .export _lcd_print
+      .export _lcd_print_char
 
 ; LCD Commands list
 LCD_CMD_CLEAR           = %00000001
@@ -191,6 +192,84 @@ _lcd_print:
       bra @lcd_print_loop
 @lcd_print_end:
       ply
+      pla
+      rts
+
+_lcd_print_char:
+      ; store register A
+      pha
+      sta tmp1
+      ; VIA1 PORTB - toggle output on 7 last bits
+      lda VIA1_DDRB
+      ora #%11111110
+      sta VIA1_DDRB
+      ; clear output for 7 last bits
+      lda VIA1_PORTB
+      and #%00000001
+      sta VIA1_PORTB
+      ; Most significant bits first
+      ; Apply mask
+      lda tmp1
+      and #%11110000
+      ; Set write data flags
+      ora #(LCD_DATA_MODE | LCD_WRITE_MODE | LCD_ENABLE_FLAG)
+      ; Send first 4 bits
+      sta VIA1_PORTB
+      ; Toggle pulse
+      eor #LCD_ENABLE_FLAG
+      sta VIA1_PORTB
+      ; Follow the same process with least significant bits
+      lda tmp1
+      and #%00001111
+      clc
+      rol
+      rol
+      rol
+      rol
+      ; Set write data flags
+      ora #(LCD_DATA_MODE | LCD_WRITE_MODE | LCD_ENABLE_FLAG)
+      ; Send first 4 bits
+      sta VIA1_PORTB
+      ; Toggle pulse
+      eor #LCD_ENABLE_FLAG
+      sta VIA1_PORTB
+
+      ;lda #01
+      ;jsr _delay_ms
+
+      lda VIA1_DDRB
+      and #%00001110
+      sta VIA1_DDRB
+@lcd_print_char_busy_flag_poll:
+      lda #(LCD_COMMAND_MODE | LCD_READ_MODE | LCD_ENABLE_FLAG)
+      sta VIA1_PORTB
+      ; Read and construct result
+      nop
+      lda VIA1_PORTB
+      and #%11110000
+      sta tmp1
+;      eor #LCD_ENABLE_FLAG
+      lda #(LCD_COMMAND_MODE | LCD_READ_MODE)
+      sta VIA1_PORTB
+      lda #(LCD_COMMAND_MODE | LCD_READ_MODE | LCD_ENABLE_FLAG)
+      sta VIA1_PORTB
+      ; Read and construct result
+      lda VIA1_PORTB
+      sta tmp2
+      eor #LCD_ENABLE_FLAG
+      sta VIA1_PORTB
+      lda tmp2
+      and #%11110000
+      clc
+      ror
+      ror
+      ror
+      ror
+      ora tmp1
+      sta tmp1
+      bmi @lcd_print_char_busy_flag_poll
+
+@lcd_print_char_end:
       pla
       rts
 
