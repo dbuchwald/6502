8819ccb55e2fc4ed268f2fb8cc997b9072061b4b
Dawid Buchwald <dawid.buchwald@hotmail.com>
2020-03-02 21:54:41 +0100

    Working version - still not refactored code

--- ROMs/firmware/common/source/lcd.s
+++ ROMs/firmware/common/source/lcd.s
@@ -133,71 +133,73 @@ _lcd_print:
       lda (lcd_out_ptr),y
       ; Exit loop if $00 read
       beq @lcd_print_end
-      ; ; Store current value, we will need it for another 4bits
-      ; pha
-      ; ; Most significant bits first
-      ; ; Apply mask
-      ; and #%11110000
-      ; ; Set write data flags
-      ; ora #(LCD_DATA_MODE | LCD_WRITE_MODE | LCD_ENABLE_FLAG)
-      ; ; Send first 4 bits
-      ; sta VIA1_PORTB
-      ; ; Toggle pulse
-      ; eor #LCD_ENABLE_FLAG
-      ; sta VIA1_PORTB
-      ; ; Follow the same process with least significant bits
-      ; pla
-      ; and #%00001111
-      ; clc
-      ; rol
-      ; rol
-      ; rol
-      ; rol
-      ; ; Set write data flags
-      ; ora #(LCD_DATA_MODE | LCD_WRITE_MODE | LCD_ENABLE_FLAG)
-      ; ; Send first 4 bits
-      ; sta VIA1_PORTB
-      ; ; Toggle pulse
-      ; eor #LCD_ENABLE_FLAG
-      ; sta VIA1_PORTB
-      sec
-      jsr lcd_write_byte
+      ; Store current value, we will need it for another 4bits
+      pha
+      ; Most significant bits first
+      ; Apply mask
+      and #%11110000
+      ; Set write data flags
+      ora #(LCD_DATA_MODE | LCD_WRITE_MODE | LCD_ENABLE_FLAG)
+      ; Send first 4 bits
+      sta VIA1_PORTB
+      ; Toggle pulse
+      eor #LCD_ENABLE_FLAG
+      sta VIA1_PORTB
+      ; Follow the same process with least significant bits
+      pla
+      and #%00001111
+      clc
+      rol
+      rol
+      rol
+      rol
+      ; Set write data flags
+      ora #(LCD_DATA_MODE | LCD_WRITE_MODE | LCD_ENABLE_FLAG)
+      ; Send first 4 bits
+      sta VIA1_PORTB
+      ; Toggle pulse
+      eor #LCD_ENABLE_FLAG
+      sta VIA1_PORTB
+      ; sec
+      ; jsr lcd_write_byte
       iny
 
       ;lda #01
       ;jsr _delay_ms
 
-      ; lda VIA1_DDRB
-      ; and #%00001111
-      ; sta VIA1_DDRB
+      lda VIA1_DDRB
+      and #%00001111
+      sta VIA1_DDRB
 @lcd_print_busy_flag_poll:
-      ; lda #(LCD_COMMAND_MODE | LCD_READ_MODE | LCD_ENABLE_FLAG)
-      ; sta VIA1_PORTB
-      ; ; Read and construct result
-      ; nop
-      ; lda VIA1_PORTB
-      ; and #%11110000
-      ; sta tmp1
-      ; ; eor #LCD_ENABLE_FLAG
-      ; lda #(LCD_COMMAND_MODE | LCD_READ_MODE)
-      ; sta VIA1_PORTB
-      ; lda #(LCD_COMMAND_MODE | LCD_READ_MODE | LCD_ENABLE_FLAG)
-      ; sta VIA1_PORTB
-      ; ; Read and construct result
-      ; lda VIA1_PORTB
-      ; sta tmp2
-      ; eor #LCD_ENABLE_FLAG
-      ; sta VIA1_PORTB
-      ; lda tmp2
-      ; and #%11110000
-      ; clc
-      ; ror
-      ; ror
-      ; ror
-      ; ror
-      ; ora tmp1
-      ; sta tmp1
-      jsr lcd_read_status
+      lda #(LCD_COMMAND_MODE | LCD_READ_MODE | LCD_ENABLE_FLAG)
+      sta VIA1_PORTB
+      ; Read and construct result
+      nop
+      lda VIA1_PORTB
+      and #%11110000
+      sta tmp1
+      ;; eor #LCD_ENABLE_FLAG
+      ;lda #(LCD_COMMAND_MODE | LCD_READ_MODE)
+      lda VIA1_PORTB
+      eor #LCD_ENABLE_FLAG
+      sta VIA1_PORTB
+      lda #(LCD_COMMAND_MODE | LCD_READ_MODE | LCD_ENABLE_FLAG)
+      sta VIA1_PORTB
+      ; Read and construct result
+      lda VIA1_PORTB
+      sta tmp2
+      eor #LCD_ENABLE_FLAG
+      sta VIA1_PORTB
+      lda tmp2
+      and #%11110000
+      clc
+      ror
+      ror
+      ror
+      ror
+      ora tmp1
+      sta tmp1
+      ; jsr lcd_read_status
       bmi @lcd_print_busy_flag_poll
       ; VIA1 PORTB - toggle output on 7 last bits
       lda VIA1_DDRB
@@ -225,66 +227,68 @@ _lcd_print_char:
       ; Most significant bits first
       ; Apply mask
       lda tmp1
-      ; and #%11110000
-      ; ; Set write data flags
-      ; ora #(LCD_DATA_MODE | LCD_WRITE_MODE | LCD_ENABLE_FLAG)
-      ; ; Send first 4 bits
-      ; sta VIA1_PORTB
-      ; ; Toggle pulse
-      ; eor #LCD_ENABLE_FLAG
-      ; sta VIA1_PORTB
-      ; ; Follow the same process with least significant bits
-      ; lda tmp1
-      ; and #%00001111
-      ; clc
-      ; rol
-      ; rol
-      ; rol
-      ; rol
-      ; ; Set write data flags
-      ; ora #(LCD_DATA_MODE | LCD_WRITE_MODE | LCD_ENABLE_FLAG)
-      ; ; Send first 4 bits
-      ; sta VIA1_PORTB
-      ; ; Toggle pulse
-      ; eor #LCD_ENABLE_FLAG
-      ; sta VIA1_PORTB
+      and #%11110000
+      ; Set write data flags
+      ora #(LCD_DATA_MODE | LCD_WRITE_MODE | LCD_ENABLE_FLAG)
+      ; Send first 4 bits
+      sta VIA1_PORTB
+      ; Toggle pulse
+      eor #LCD_ENABLE_FLAG
+      sta VIA1_PORTB
+      ; Follow the same process with least significant bits
+      lda tmp1
+      and #%00001111
+      clc
+      rol
+      rol
+      rol
+      rol
+      ; Set write data flags
+      ora #(LCD_DATA_MODE | LCD_WRITE_MODE | LCD_ENABLE_FLAG)
+      ; Send first 4 bits
+      sta VIA1_PORTB
+      ; Toggle pulse
+      eor #LCD_ENABLE_FLAG
+      sta VIA1_PORTB
 
       ; ;lda #01
       ; ;jsr _delay_ms
-      sec
-      jsr lcd_write_byte
+      ; sec
+      ; jsr lcd_write_byte
 
-      ; lda VIA1_DDRB
-      ; and #%00001110
-      ; sta VIA1_DDRB
+      lda VIA1_DDRB
+      and #%00001110
+      sta VIA1_DDRB
 @lcd_print_char_busy_flag_poll:
-      ; lda #(LCD_COMMAND_MODE | LCD_READ_MODE | LCD_ENABLE_FLAG)
-      ; sta VIA1_PORTB
-      ; ; Read and construct result
-      ; nop
-      ; lda VIA1_PORTB
-      ; and #%11110000
-      ; sta tmp1
-      ; ; eor #LCD_ENABLE_FLAG
-      ; lda #(LCD_COMMAND_MODE | LCD_READ_MODE)
-      ; sta VIA1_PORTB
-      ; lda #(LCD_COMMAND_MODE | LCD_READ_MODE | LCD_ENABLE_FLAG)
-      ; sta VIA1_PORTB
-      ; ; Read and construct result
-      ; lda VIA1_PORTB
-      ; sta tmp2
-      ; eor #LCD_ENABLE_FLAG
-      ; sta VIA1_PORTB
-      ; lda tmp2
-      ; and #%11110000
-      ; clc
-      ; ror
-      ; ror
-      ; ror
-      ; ror
-      ; ora tmp1
-      ; sta tmp1
-      jsr lcd_read_status
+      lda #(LCD_COMMAND_MODE | LCD_READ_MODE | LCD_ENABLE_FLAG)
+      sta VIA1_PORTB
+      ; Read and construct result
+      nop
+      lda VIA1_PORTB
+      and #%11110000
+      sta tmp1
+      ;; eor #LCD_ENABLE_FLAG
+      ;lda #(LCD_COMMAND_MODE | LCD_READ_MODE)
+      lda VIA1_PORTB
+      eor #LCD_ENABLE_FLAG
+      sta VIA1_PORTB
+      lda #(LCD_COMMAND_MODE | LCD_READ_MODE | LCD_ENABLE_FLAG)
+      sta VIA1_PORTB
+      ; Read and construct result
+      lda VIA1_PORTB
+      sta tmp2
+      eor #LCD_ENABLE_FLAG
+      sta VIA1_PORTB
+      lda tmp2
+      and #%11110000
+      clc
+      ror
+      ror
+      ror
+      ror
+      ora tmp1
+      sta tmp1
+      ; jsr lcd_read_status
       bmi @lcd_print_char_busy_flag_poll
 
 @lcd_print_char_end:
