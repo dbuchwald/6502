57d8bda4b220ba454e23ae9190f8b35ed5c442b6
Dawid Buchwald <dawid.buchwald@hotmail.com>
2020-03-18 13:18:05 +0100

    Added support for special characters

--- ROMs/firmware/19_keyboard_test/keyboard_test.s
+++ ROMs/firmware/19_keyboard_test/keyboard_test.s
@@ -3,11 +3,6 @@
       .include "via.inc"
       .include "lcd.inc"
       .include "zeropage.inc"
-      .import __RAM_START__
-
-WRITE_INDEX   = __RAM_START__
-MEM_BUFFER    = __RAM_START__+1
-
       .segment "VECTORS"
 
       .word   $0000
@@ -30,10 +25,6 @@ handshake_init:
       lda #(VIA_IER_SET_FLAGS | VIA_IER_CA1_FLAG)
       sta VIA1_IER
 
-      ldx #00                  ; set display index to 0
-      stz WRITE_INDEX          ; set buffer index to 0
-      ldy #00                  ; set internal buffer pointer to 0
-
       jsr _lcd_init
 
       lda #$ff
@@ -41,33 +32,53 @@ handshake_init:
       lda VIA1_IFR
       lda VIA1_PORTA
 
-      cli                      ; enable interrupt processing
+      write_lcd hello_msg
+      lda #01
+      jsr _delay_sec
+      lda #('.')
+      jsr _lcd_print_char
+      lda #01
+      jsr _delay_sec
+      lda #('.')
+      jsr _lcd_print_char
+      lda #01
+      jsr _delay_sec
 
-      lda #('?')
-      jsr _lcd_print_char
-      lda #(' ')
-      jsr _lcd_print_char
+      cli                      ; enable interrupt processing
 
 program_loop:
       jmp program_loop
-      ; cpx WRITE_INDEX
-      ; beq program_loop
-      ; inx
-      ; lda MEM_BUFFER,x         ; Load data bytes from address data + x
-      ; jsr _lcd_print_char
 
 read_data:
       pha
       lda VIA1_PORTA           ; should trigger data taken signal
-      ; sta MEM_BUFFER,y
-      ; iny
-      ; sty WRITE_INDEX
       cmp #$ff
       beq keyboard_connected
       cmp #$fe
       beq keyboard_disconnected
       jsr _lcd_clear
       write_lcd key_msg
+      ldx #$00
+special_keys_loop:
+      cmp special_keys,x
+      beq print_special_key
+      inx
+      inx
+      inx
+      ldy special_keys,x
+      cpy #$00
+      bne special_keys_loop
+      bra regular_char
+print_special_key:
+      inx
+      lda special_keys,x
+      sta lcd_out_ptr
+      inx
+      lda special_keys,x
+      sta lcd_out_ptr+1
+      jsr _lcd_print
+      bra read_data_end
+regular_char:
       jsr _lcd_print_char
       write_lcd key_part_two
       jsr _convert_to_hex
@@ -93,13 +104,54 @@ read_data_end:
       rti
 
       .segment "RODATA"
+hello_msg:
+      .byte "Keyboard testing program.", $00
 key_msg:
       .byte "Key pressed: ",$00
 key_part_two:
       .byte " ($",$00
 key_part_three:
       .byte ")",$00
+
 conn_msg:
       .byte "Keyboard connected", $00
 disconn_msg:
       .byte "Keyboard disconnected", $00
+
+special_keys:
+      .macro special_char_def code, string_addr
+      .byte code, <string_addr, >string_addr
+      .endmacro
+
+      special_char_def $0d, special_enter
+      special_char_def $1b, special_esc
+      special_char_def $7f, special_back
+      special_char_def $0b, special_up
+      special_char_def $0a, special_down
+      special_char_def $15, special_right
+      special_char_def $08, special_left
+      special_char_def $09, special_tab
+      special_char_def $19, special_pgup
+      special_char_def $1a, special_pgdown
+      special_char_def $00, $0000
+
+special_enter:
+      .byte "[ENTER]",$00
+special_back:
+      .byte "[BACK]",$00
+special_esc:
+      .byte "[ESC]",$00
+special_up:
+      .byte "[UP]",$00
+special_down:
+      .byte "[DOWN]",$00
+special_left:
+      .byte "[LEFT]",$00
+special_right:
+      .byte "[RIGHT]",$00
+special_tab:
+      .byte "[TAB]",$00
+special_pgup:
+      .byte "[PGUP]",$00
+special_pgdown:
+      .byte "[PGDN]",$00
