473c001bcb568868ed95051b49a6a61a80e55eff
Dawid Buchwald <dawid.buchwald@hotmail.com>
2020-03-20 16:08:00 +0100

    Added trim program for load modules

---
+++ Software/common/loadtrim.py
@@ -0,0 +1,27 @@
+import sys
+
+in_file=open(sys.argv[1], "rb")
+
+binary_input=in_file.read()
+in_file.close()
+
+nonzero_index = len(binary_input)-1
+
+while (ord(binary_input[nonzero_index]) == 0):
+  nonzero_index=nonzero_index-1
+
+binary_output = bytearray([0x00] * (nonzero_index+3))
+
+for copy_index in range(nonzero_index+1):
+  binary_output[copy_index+2] = binary_input[copy_index]
+
+binary_output[0x0000]=0x00
+binary_output[0x0001]=0x10
+
+out_file=open(sys.argv[2], "wb")
+
+out_file.write(binary_output)
+
+out_file.close()
+
+
