023a9cb58dfc8387229a93990f6f1baa3daf15b3
Dawid Buchwald <dawid.buchwald@cgi.com>
2020-04-03 16:56:59 +0200

    First implementation of customizable menu

---
+++ Software/load/10_menu/menu.s
@@ -0,0 +1,286 @@
+        .setcpu "65C02"
+        .include "tty.inc"
+        .include "string.inc"
+        .include "utils.inc"
+
+CMD_OFFSET      = $00
+ARGC_OFFSET     = $01
+DESC_OFFSET     = $02
+FUNCTION_OFFSET = $03
+NEXT_OFFSET     = $04
+
+        .macro is_last_menu_item pointer
+        .local not_last
+        .local return
+        pha
+        phy
+        lda ptr1
+        pha
+        lda ptr1+1
+        pha
+        lda pointer
+        sta ptr1
+        lda pointer+1
+        sta ptr1+1
+        ldy #$00
+        lda (ptr1),y
+        bne not_last
+        sec
+        bra return
+not_last:
+        clc
+return:        
+        pla
+        sta ptr1+1
+        pla
+        sta ptr1
+        ply
+        pla
+        .endmacro
+
+        .macro get_next_menu_item pointer
+        pha
+        phy
+        lda ptr1
+        pha
+        lda ptr1+1
+        pha
+        lda pointer
+        sta ptr1
+        lda pointer+1
+        sta ptr1+1
+        ldy #(NEXT_OFFSET)
+        lda (ptr1),y
+        clc
+        adc ptr1
+        sta pointer
+        lda ptr1+1
+        adc #$00
+        sta pointer+1
+        pla
+        sta ptr1+1
+        pla
+        sta ptr1
+        ply
+        pla
+        .endmacro
+
+        .macro get_menu_item pointer, cmd_pointer, argc, desc_pointer, function_pointer
+        pha
+        phy
+        lda ptr1
+        pha
+        lda ptr1+1
+        pha
+        lda pointer
+        sta ptr1
+        lda pointer+1
+        sta ptr1+1
+        ldy #(CMD_OFFSET)
+        lda (ptr1),y
+        clc
+        adc ptr1
+        sta cmd_pointer
+        lda ptr1+1
+        adc #$00
+        sta cmd_pointer+1
+        ldy #(ARGC_OFFSET)
+        lda (ptr1),y
+        sta argc
+        ldy #(DESC_OFFSET)
+        lda (ptr1),y
+        clc
+        adc ptr1
+        sta desc_pointer
+        lda ptr1+1
+        adc #$00
+        sta desc_pointer+1
+        ldy #(FUNCTION_OFFSET)
+        lda (ptr1),y
+        clc
+        adc ptr1
+        sta ptr1
+        lda ptr1+1
+        adc #$00
+        sta ptr1+1
+        ldy #$00
+        lda (ptr1),y
+        sta function_pointer
+        iny
+        lda (ptr1),y
+        sta function_pointer+1
+        pla
+        sta ptr1+1
+        pla
+        sta ptr1
+        ply
+        pla
+        .endmacro
+
+        .export _run_menu
+
+LINE_BUFFER_SIZE = 32
+TOKENIZE_BUFFER_SIZE = 64
+
+        .code
+
+_run_menu:
+        copy_ptr ptr1, menu_root
+        copy_ptr ptr2, menu_prompt
+
+main_loop:
+        ; Display prompt
+        write_tty_p menu_prompt
+
+        ; Read one line
+        tty_read_line line_buffer, LINE_BUFFER_SIZE
+
+        ; Trim trailing and leading whitespace
+        strtriml line_buffer
+        strtrimr line_buffer
+
+        ; If empty - repeat
+        strlen line_buffer
+        cmp #$00
+        beq main_loop
+
+        ; Tokenize input
+        strtokenize line_buffer, tokenize_buffer, TOKENIZE_BUFFER_SIZE
+        sta tokens_count
+
+        ; Convert first token to uppercase for comparison
+        strtoupper tokenize_buffer
+
+        ; Start from the beginning
+        copy_ptr menu_root, menu_item
+menu_commands_loop:
+        is_last_menu_item menu_item
+        bcc compare_item
+        jmp special_commands
+compare_item:
+        get_menu_item menu_item, menu_item_cmd, menu_item_argc, menu_item_desc, menu_item_function
+        strcmpp tokenize_buffer_pointer, menu_item_cmd
+        cmp #$00
+        bne next_menu_item
+        ; check number of parameters
+        lda menu_item_argc
+        cmp tokens_count
+        bne next_menu_item
+        ; preserve critical local variables in case callee
+        ; invokes his own menu
+        lda menu_root
+        pha
+        lda menu_root+1
+        pha
+        lda menu_prompt
+        pha
+        lda menu_prompt+1
+        pha
+        ; execute function
+        jsr execute_menu_function
+        ; restore critical variables
+        pla
+        sta menu_prompt+1
+        pla
+        sta menu_prompt
+        pla
+        sta menu_root+1
+        pla
+        sta menu_root
+        ; return to menu
+        jmp main_loop
+next_menu_item:
+        get_next_menu_item menu_item
+        jmp menu_commands_loop
+special_commands:
+        lda tokens_count
+        cmp #$01
+        bne invalid_command
+        strcmpp tokenize_buffer_pointer, cmd_help_ptr
+        cmp #$00
+        bne not_help
+        jsr display_help_message
+        jmp main_loop
+not_help:
+        strcmpp tokenize_buffer_pointer, cmd_exit_ptr
+        cmp #$00
+        bne invalid_command
+        writeln_tty byemsg
+        rts
+invalid_command:
+        writeln_tty errormsg1
+        writeln_tty errormsg2
+        jmp main_loop
+
+execute_menu_function:
+        ; provide access to parameters
+        copy_ptr tokenize_buffer_pointer, ptr1
+        jmp (menu_item_function)
+
+display_help_message:
+        writeln_tty helpmsg1
+        writeln_tty helpmsg2
+        copy_ptr menu_root, menu_item
+help_loop:
+        is_last_menu_item menu_item
+        bcc display_item
+        jmp done_listing
+display_item:
+        get_menu_item menu_item, menu_item_cmd, menu_item_argc, menu_item_desc, menu_item_function
+
+        write_tty helpind
+        writeln_tty_p menu_item_desc
+
+        get_next_menu_item menu_item
+        jmp help_loop
+done_listing:
+        writeln_tty helpmsg3
+        rts
+
+        .segment "BSS"
+line_buffer:
+        .res LINE_BUFFER_SIZE
+tokenize_buffer:
+        .res TOKENIZE_BUFFER_SIZE
+tokens_count:
+        .res 1
+menu_root:
+        .res 2
+menu_prompt:
+        .res 2
+menu_item:
+        .res 2
+menu_item_cmd:
+        .res 2
+menu_item_argc:
+        .res 1
+menu_item_desc:
+        .res 2
+menu_item_function:
+        .res 2
+
+        .segment "RODATA"
+cmd_help:
+        .asciiz "HELP"
+cmd_exit:
+        .asciiz "EXIT"
+cmd_help_ptr:
+        .word cmd_help
+cmd_exit_ptr:
+        .word cmd_exit
+tokenize_buffer_pointer:
+        .word tokenize_buffer
+errormsg1:
+        .asciiz "Command not recognized - either wrong keyword or incorrect number of parameters"
+errormsg2:
+        .asciiz "Enter HELP command to list available commands"
+byemsg:
+        .asciiz "Exiting..."
+helpmsg1:
+        .asciiz "The following commands are available:"
+helpmsg2:
+        .asciiz "  HELP - display this information"
+helpmsg3:
+        .asciiz "  EXIT - exit the menu"
+helpind:
+        .asciiz "  "
--- Software/load/10_menu/menu_test.s
+++ Software/load/10_menu/menu_test.s
@@ -1,69 +1,143 @@
         .setcpu "65C02"
         .include "tty.inc"
-        .include "string.inc"
-        .include "utils.inc"
-        .include "acia.inc"
         .include "menu.inc"
-        .include "debug.inc"
+        .include "utils.inc"
+        .include "string.inc"
+        .include "blink.inc"
+
+        .macro gettoken token_buffer_pointer, number
+        .local token_loop
+        .local char_loop
+        .local exit
+        lda #(number)
+        sta tmp1
+        copy_ptr token_buffer_pointer, ptr1
+        ldy #$00
+token_loop:
+        lda tmp1
+        beq exit
+char_loop:
+        inc_ptr ptr1
+        lda (ptr1),y
+        bne char_loop
+        inc_ptr ptr1
+        dec tmp1
+        bra token_loop
+exit:
+        .endmacro
 
         .code
 init:
-        lda #<menu
-        sta menu_item
-        lda #>menu
-        sta menu_item+1
-
-main_loop:
-        ; writeln_tty_address menu_item
-
-        is_last_menu_item menu_item
-        bcc display_item
-        jmp done_listing
-display_item:
-        get_menu_item menu_item, menu_item_cmd, menu_item_argc, menu_item_desc
-
-        ; writeln_tty_address menu_item_cmd
-        ; writeln_tty_address menu_item_argc
-        ; writeln_tty_address menu_item_desc
-        
-        writeln_tty msgh
-        write_tty msgc
-        writeln_tty_p menu_item_cmd
-        write_tty msga
-        ; writeln_tty msgh
-        lda menu_item_argc
-        write_tty_hex 
-        writeln_tty empty
-
-        write_tty msgd
-        writeln_tty_p menu_item_desc
-
-        get_next_menu_item menu_item
-        jmp main_loop
-done_listing:
+        run_menu menu, test_prompt
         rts
 
+process_get_2:
+        copy_ptr ptr1, tokens_pointer
+        writeln_tty get2msg
+        lda #$02
+        sta tmp1
+        jsr list_params
+        rts
+
+process_get_4:
+        copy_ptr ptr1, tokens_pointer
+        writeln_tty get4msg
+        lda #$04
+        sta tmp1
+        jsr list_params
+        rts
+
+process_blink:
+        copy_ptr ptr1, tokens_pointer
+        writeln_tty get4msg
+        lda #$02
+        sta tmp1
+        jsr list_params
+
+        gettoken tokens_pointer, 1
+        copy_ptr ptr1, param_pointer
+        strtoupperp param_pointer
+        strcmpp blinkon_ptr, param_pointer
+        cmp #$00
+        bne not_on
+        sec
+        jsr _blink_led
+        rts
+not_on:
+        strcmpp blinkoff_ptr, param_pointer
+        cmp #$00
+        bne error
+        clc
+        jsr _blink_led
+        rts
+error:
+        writeln_tty blinkerror
+        rts
+
+
+process_put:
+        writeln_tty putmsg
+        rts
+
+list_params:
+        copy_ptr tokens_pointer, ptr2
+        ldy #$00
+list_loop:
+        write_tty paramval
+        writeln_tty_p ptr2
+next_token_loop:
+        inc ptr2
+        bne check_for_end
+        inc ptr2+1
+check_for_end:
+        lda (ptr2),y
+        beq end_found
+        bra next_token_loop
+end_found:
+        inc ptr2
+        bne repeat_loop
+        inc ptr2+1
+repeat_loop:
+        dec tmp1
+        bne list_loop
+        rts
+
+
+        
         .segment "BSS"
-menu_item:
+tokens_pointer:
         .res 2
-menu_item_cmd:
-        .res 2
-menu_item_argc:
-        .res 1
-menu_item_desc:
+param_pointer:
         .res 2
 
         .segment "RODATA"
-msgh:   .asciiz "Menu item found"
-msgc:   .asciiz "  command: "
-msga:   .asciiz "  argc: "
-msgd:   .asciiz "  description: "
-
-empty:  .asciiz "" 
 
 menu:
-        menuitem help, "HELP", 1, "HELP - display this message", _display_help_message
-        menuitem get2, "GET", 2, "GET - get data at the address", _display_exit_message
-        menuitem get4, "GET", 4, "GET - get data between addresses", _display_exit_message
-        menuitem exit, "EXIT", 1, "EXIT - exit monitor", _display_exit_message
+        menuitem get2, "GET", 2, "GET xxxx - get data at the address", process_get_2
+        menuitem get4, "GET", 4, "GET xxxx:yyyy - get data between addresses", process_get_4
+        menuitem put, "PUT", 1, "PUT - put data at the address", process_put
+        menuitem blink, "BLINK", 2, "BLINK on/off - toggle onboard blink LED", process_blink
         endmenu 
+
+test_prompt:
+        .asciiz "OS/1 Test Menu>"
+get2msg:
+        .asciiz "Invoked GET with two parameters"
+get4msg:
+        .asciiz "Invoked GET with four parameters"
+putmsg:
+        .asciiz "Invoked PUT"
+blinkmsg:
+        .asciiz "Invoked BLINK"
+blinkerror:
+        .asciiz "Incorrect parameters passed!"
+paramval:
+        .asciiz "  Parameter: "
+blinkon:
+        .asciiz "ON"
+blinkoff:
+        .asciiz "OFF"
+blinkon_ptr:
+        .word blinkon
+blinkoff_ptr:
+        .word blinkoff
