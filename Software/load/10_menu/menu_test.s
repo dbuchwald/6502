0d923091168da6a01ac25201ec640d426c56ac5e
Dawid Buchwald <dawid.buchwald@cgi.com>
2020-04-04 14:42:08 +0200

    Implemented basic parse functions

--- Software/load/10_menu/menu_test.s
+++ Software/load/10_menu/menu_test.s
@@ -4,6 +4,7 @@
         .include "utils.inc"
         .include "string.inc"
         .include "blink.inc"
+        .include "parse.inc"
 
         .macro gettoken token_buffer_pointer, number
         .local token_loop
@@ -49,28 +50,84 @@ process_get_4:
 
 process_blink:
         copy_ptr ptr1, tokens_pointer
-        writeln_tty get4msg
+        writeln_tty blinkmsg
         lda #$02
         sta tmp1
         jsr list_params
 
         gettoken tokens_pointer, 1
         copy_ptr ptr1, param_pointer
-        strtoupperp param_pointer
-        strcmpp blinkon_ptr, param_pointer
+
+        parse_onoff_p param_pointer
+        bcc blink_error
         cmp #$00
-        bne not_on
+        beq turn_off
         sec
         jsr _blink_led
         rts
-not_on:
-        strcmpp blinkoff_ptr, param_pointer
-        cmp #$00
-        bne error
+turn_off:
         clc
         jsr _blink_led
         rts
-error:
+blink_error:
+        writeln_tty blinkerror
+        rts
+
+
+process_print:
+        copy_ptr ptr1, tokens_pointer
+        writeln_tty printmsg
+        lda #$02
+        sta tmp1
+        jsr list_params
+
+        gettoken tokens_pointer, 1
+        copy_ptr ptr1, param_pointer
+
+        parse_hex_byte_p param_pointer
+        bcc @error
+        jsr _convert_to_hex
+        txa
+        sta printplace
+        tya
+        sta printplace+1
+        
+        writeln_tty printparsed
+
+        rts
+@error:
+        writeln_tty blinkerror
+        rts
+
+process_addr:
+        copy_ptr ptr1, tokens_pointer
+        writeln_tty addrmsg
+        lda #$02
+        sta tmp1
+        jsr list_params
+
+        gettoken tokens_pointer, 1
+        copy_ptr ptr1, param_pointer
+
+        parse_hex_word_p param_pointer
+        bcc @error
+        phx
+        jsr _convert_to_hex
+        txa
+        sta addrplace
+        tya
+        sta addrplace+1
+        pla
+        jsr _convert_to_hex
+        txa
+        sta addrplace+2
+        tya
+        sta addrplace+3
+        
+        writeln_tty addrparsed
+
+        rts
+@error:
         writeln_tty blinkerror
         rts
 
@@ -117,6 +174,8 @@ menu:
         menuitem get4, "GET", 4, "GET xxxx:yyyy - get data between addresses", process_get_4
         menuitem put, "PUT", 1, "PUT - put data at the address", process_put
         menuitem blink, "BLINK", 2, "BLINK on/off - toggle onboard blink LED", process_blink
+        menuitem print, "PRINT", 2, "PRINT xx - parse and print back hex numer", process_print
+        menuitem print, "ADDR", 2, "ADDR xxxx - parse and print back hex address", process_addr
         endmenu 
 
 test_prompt:
@@ -131,13 +190,18 @@ blinkmsg:
         .asciiz "Invoked BLINK"
 blinkerror:
         .asciiz "Incorrect parameters passed!"
+printmsg:
+        .asciiz "Invoked PRINT"
+addrmsg:
+        .asciiz "Invoked ADDR"
+printparsed:
+        .byte "Received $"
+printplace:
+        .asciiz "xx "
+addrparsed:
+        .byte "Received 0x"
+addrplace:
+        .asciiz "xxxx "
 paramval:
         .asciiz "  Parameter: "
-blinkon:
-        .asciiz "ON"
-blinkoff:
-        .asciiz "OFF"
-blinkon_ptr:
-        .word blinkon
-blinkoff_ptr:
-        .word blinkoff
+
---
+++ Software/load/10_menu/parse.s
@@ -0,0 +1,183 @@
+        .setcpu "65C02"
+        .include "string.inc"
+        .include "utils.inc"
+
+        .export _parse_onoff_p
+        .export _parse_hex_byte_p
+        .export _parse_hex_word_p
+
+; Function assumes token pointer in ptr1
+; Returns status in carry flag (set - parsed successfully)
+; Returns value in A (0 - off, 1 - on)
+_parse_onoff_p:
+        ; Copy pointer to preserve during strcmp operation
+        copy_ptr ptr1, parsed_token_pointer
+        ; Convert whole token uppercase for comparison
+        strtoupperp parsed_token_pointer
+        ; Compare against "ON"
+        strcmpp token_on_ptr, parsed_token_pointer
+        cmp #$00
+        bne @not_on
+        ; Equal
+        sec
+        lda #$01
+        rts
+@not_on:
+        ; Compare against "OFF"
+        strcmpp token_off_ptr, parsed_token_pointer
+        cmp #$00
+        bne @error
+        sec
+        lda #$00
+        rts
+@error:
+        clc
+        rts
+
+; Function assumes token pointer in ptr1
+; Returns status in carry flag (set - parsed successfully)
+; Returns value in A
+; Stores temp value in tmp1 (but preserves it)
+_parse_hex_byte_p:
+        ; preserve Y
+        phy
+        ; preserve tmp1
+        lda tmp1
+        pha
+        ; Copy pointer to preserve during strtoupper operation
+        copy_ptr ptr1, parsed_token_pointer
+        ; Convert whole token uppercase for comparison
+        strtoupperp parsed_token_pointer
+        ; Copy pointer back to dereference
+        copy_ptr parsed_token_pointer, ptr1
+        ; get first char
+        ldy #$00
+        lda (ptr1),y
+        jsr _parse_hex_single_char
+        bcc @error
+        asl
+        asl
+        asl
+        asl
+        sta tmp1
+        iny
+        lda (ptr1),y
+        jsr _parse_hex_single_char
+        bcc @error
+        clc
+        adc tmp1
+        sec
+        bra @return
+@error:
+        clc
+@return:
+        ply
+        sty tmp1
+        ply
+        rts
+
+; Function assumes token pointer in ptr1
+; Returns status in carry flag (set - parsed successfully)
+; Returns value in MSB: A, LSB: X
+; Stores temp value in tmp1 and tmp2 (but preserves them)
+_parse_hex_word_p:
+        ; preserve Y
+        phy
+        ; preserve tmp1
+        lda tmp1
+        pha
+        lda tmp2
+        pha
+        ; Copy pointer to preserve during strtoupper operation
+        copy_ptr ptr1, parsed_token_pointer
+        ; Convert whole token uppercase for comparison
+        strtoupperp parsed_token_pointer
+        ; Copy pointer back to dereference
+        copy_ptr parsed_token_pointer, ptr1
+        ; get first char
+        ldy #$00
+        lda (ptr1),y
+        jsr _parse_hex_single_char
+        bcc @error
+        asl
+        asl
+        asl
+        asl
+        sta tmp1
+        iny
+        lda (ptr1),y
+        jsr _parse_hex_single_char
+        bcc @error
+        clc
+        adc tmp1
+        sta tmp1
+        iny
+        lda (ptr1),y
+        jsr _parse_hex_single_char
+        bcc @error
+        asl
+        asl
+        asl
+        asl
+        sta tmp2
+        iny
+        lda (ptr1),y
+        jsr _parse_hex_single_char
+        bcc @error
+        clc
+        adc tmp2
+        sta tmp2
+        sec
+        ldx tmp2
+        lda tmp1
+        bra @return
+@error:
+        clc
+@return:
+        ply
+        sty tmp2
+        ply
+        sty tmp1
+        ply
+        rts
+
+
+; Assumes char in A
+; returns status in carry (set - OK)
+; returns value in A
+_parse_hex_single_char:
+        cmp #('0')
+        bmi @error 
+        cmp #('9'+1)
+        bmi @digit
+        cmp #('A')
+        bmi @error
+        cmp #('F'+1)
+        bmi @char
+        bra @error
+@digit:
+        sec
+        sbc #('0')
+        sec
+        rts
+@char:
+        sec
+        sbc #('A'-$0a)
+        sec
+        rts
+@error:
+        clc
+        rts
+
+        .segment "BSS"
+parsed_token_pointer:
+        .res 2
+        .segment "RODATA"
+token_on:
+        .asciiz "ON"
+token_off:
+        .asciiz "OFF"
+token_on_ptr:
+        .word token_on
+token_off_ptr:
+        .word token_off
