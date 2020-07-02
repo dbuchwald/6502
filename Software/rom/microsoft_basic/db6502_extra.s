b681b6dda968f52b7e9b0742a5d24217778f4bed
Dawid Buchwald <dawid.buchwald@cgi.com>
2020-07-02 12:40:04 +0200

    First integration with some OS/1 routines

--- Software/rom/microsoft_basic/db6502_extra.s
+++ Software/rom/microsoft_basic/db6502_extra.s
@@ -1,66 +1,10 @@
-__ACIA_START__ = $8400
-
-ACIA_DATA    = __ACIA_START__ + $00
-ACIA_STATUS  = __ACIA_START__ + $01
-ACIA_COMMAND = __ACIA_START__ + $02
-ACIA_CONTROL = __ACIA_START__ + $03
-
-ACIA_STOP_BITS_1 = %00000000
-ACIA_STOP_BITS_2 = %10000000
-ACIA_DATA_BITS_8 = %00000000
-ACIA_DATA_BITS_7 = %00100000
-ACIA_DATA_BITS_6 = %01000000
-ACIA_DATA_BITS_5 = %01100000
-ACIA_CLOCK_EXT   = %00000000
-ACIA_CLOCK_INT   = %00010000
-ACIA_BAUD_16XEXT = %00000000
-ACIA_BAUD_50     = %00000001
-ACIA_BAUD_75     = %00000010
-ACIA_BAUD_109    = %00000011
-ACIA_BAUD_134    = %00000100
-ACIA_BAUD_150    = %00000101
-ACIA_BAUD_300    = %00000110
-ACIA_BAUD_600    = %00000111
-ACIA_BAUD_1200   = %00001000
-ACIA_BAUD_1800   = %00001001
-ACIA_BAUD_2400   = %00001010
-ACIA_BAUD_3600   = %00001011
-ACIA_BAUD_4800   = %00001100
-ACIA_BAUD_7200   = %00001101
-ACIA_BAUD_9600   = %00001110
-ACIA_BAUD_19200  = %00001111
-
-; ACIA command register bit values
-
-ACIA_PARITY_ODD              = %00000000
-ACIA_PARITY_EVEN             = %01000000
-ACIA_PARITY_MARK             = %10000000
-ACIA_PARITY_SPACE            = %11000000
-ACIA_PARITY_DISABLE          = %00000000
-ACIA_PARITY_ENABLE           = %00100000
-ACIA_ECHO_DISABLE            = %00000000
-ACIA_ECHO_ENABLE             = %00010000
-ACIA_TX_INT_DISABLE_RTS_HIGH = %00000000
-ACIA_TX_INT_ENABLE_RTS_LOW   = %00000100
-ACIA_TX_INT_DISABLE_RTS_LOW  = %00001000
-ACIA_TX_INT_DISABLE_BREAK    = %00001100
-ACIA_RX_INT_ENABLE           = %00000000
-ACIA_RX_INT_DISABLE          = %00000010
-ACIA_DTR_HIGH                = %00000000
-ACIA_DTR_LOW                 = %00000001
-
-; ACIA status register bit masks
-
-ACIA_STATUS_IRQ        = 1 << 7
-ACIA_STATUS_DSR        = 1 << 6
-ACIA_STATUS_DCD        = 1 << 5
-ACIA_STATUS_TX_EMPTY   = 1 << 4
-ACIA_STATUS_RX_FULL    = 1 << 3
-ACIA_STATUS_OVERRUN    = 1 << 2
-ACIA_STATUS_FRAME_ERR  = 1 << 1
-ACIA_STATUS_PARITY_ERR = 1 << 0
-
-
+      .include "utils.inc"
+      .include "lcd.inc"
+      .include "core.inc"
+      .include "acia.inc"
+      .include "keyboard.inc"
+      .include "syscalls.inc"
+      .include "tty.inc"
 
 .segment "CODE"
 ; ISCNTC:
@@ -82,10 +26,17 @@ init:
       ldx #$ff
       txs
 
-      lda #(ACIA_PARITY_DISABLE | ACIA_ECHO_DISABLE | ACIA_TX_INT_DISABLE_RTS_LOW | ACIA_RX_INT_DISABLE | ACIA_DTR_LOW)
-      sta ACIA_COMMAND
-      lda #(ACIA_STOP_BITS_1 | ACIA_DATA_BITS_8 | ACIA_CLOCK_INT | ACIA_BAUD_19200)
-      sta ACIA_CONTROL
+      jsr _system_init
+
+      write_lcd #ms_basic
+
+      lda #(TTY_CONFIG_INPUT_SERIAL | TTY_CONFIG_INPUT_KEYBOARD | TTY_CONFIG_OUTPUT_SERIAL)
+      jsr _tty_init      
+
+      ; lda #(ACIA_PARITY_DISABLE | ACIA_ECHO_DISABLE | ACIA_TX_INT_DISABLE_RTS_LOW | ACIA_RX_INT_DISABLE | ACIA_DTR_LOW)
+      ; sta ACIA_COMMAND
+      ; lda #(ACIA_STOP_BITS_1 | ACIA_DATA_BITS_8 | ACIA_CLOCK_INT | ACIA_BAUD_19200)
+      ; sta ACIA_CONTROL
 
 ; Display startup message
 	LDY #0
@@ -114,24 +65,34 @@ WarmStart:
 	JMP	RESTART		; BASIC warm start
 
 MONCOUT:
-	PHA
-SerialOutWait:
-	LDA	ACIA_STATUS
-	AND	#ACIA_STATUS_TX_EMPTY
-	BEQ	SerialOutWait
-	PLA
-	STA	ACIA_DATA
+;	PHA
+; SerialOutWait:
+; 	LDA	ACIA_STATUS
+; 	AND	#ACIA_STATUS_TX_EMPTY
+; 	BEQ	SerialOutWait
+; 	PLA
+; 	STA	ACIA_DATA
+    jsr _tty_send_character
 	RTS
 
 MONRDKEY:
-	LDA	ACIA_STATUS
-	AND	#ACIA_STATUS_RX_FULL
-	BEQ	NoDataIn
-	LDA	ACIA_DATA
-	SEC		; Carry set if key available
-	RTS
+; 	LDA	ACIA_STATUS
+; 	AND	#ACIA_STATUS_RX_FULL
+; 	BEQ	NoDataIn
+; 	LDA	ACIA_DATA
+; 	SEC		; Carry set if key available
+; 	RTS
+; NoDataIn:
+; 	CLC		; Carry clear if no key pressed
+  jsr _acia_is_data_available
+  ; skip, no data available at this point
+  cmp #(ACIA_NO_DATA_AVAILABLE)
+  beq NoDataIn
+  jsr _acia_read_byte
+  sec
+  rts
 NoDataIn:
-	CLC		; Carry clear if no key pressed
+  clc
 	RTS
 
 MONISCNTC:
@@ -147,8 +108,7 @@ NotCTRLC:
 
 SEND_BACKSPACE:
   PHA
-  TXA
-  PHA
+  PHX
   LDX #$FF
 BackspaceLoop:
   INX
@@ -157,8 +117,7 @@ BackspaceLoop:
   JSR MONCOUT
   BNE BackspaceLoop
 BackspaceLoopEnd:
-  PLA
-  TAX
+  PLX
   PLA
   RTS
 
@@ -167,6 +126,9 @@ StartupMessage:
 
 Backspace:
   .byte $1B,"[D ",$1B,"[D",$00
+
+ms_basic:
+  .asciiz "Microsoft BASIC"
 
 LOAD:
 	RTS
@@ -178,4 +140,4 @@ SAVE:
 
       .word   $0000
       .word   init
-      .word   $0000
+      .word   _interrupt_handler
