f3d49c436323fcc72301ca3b029023a5f729a1f2
Dawid Buchwald <dawid.buchwald@cgi.com>
2020-07-06 19:14:37 +0200

    Preparing config for USBAsp

---
+++ AVR/blink-mega2560-usbasp/main.c
@@ -0,0 +1,18 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    // Configure PB5 as an output using the Port B Data Direction Register (DDRB)
+    DDRB |= _BV(DDB7);
+
+    // Loop forever
+    while (1)
+    {
+        // Toggle PB5 using the Port B Input Pin Register (PINB)
+        PINB = _BV(PINB7);
+
+        // Wait 500ms
+        _delay_ms(200);
+    }
+}
