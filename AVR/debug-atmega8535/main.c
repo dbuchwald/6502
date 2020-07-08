e1897cc3fe71f3986e9dcbe129afa2deecbb1c32
Dawid Buchwald <dawid.buchwald@hotmail.com>
2020-07-08 22:43:02 +0200

    Fixed port inputs (they do work now)

--- AVR/debug-atmega8535/main.c
+++ AVR/debug-atmega8535/main.c
@@ -50,12 +50,19 @@ int main(void)
     // port A and C are inputs for Address Bus
     DDRA = 0x00;
     DDRC = 0x00;
+    // Enable all pull-ups
+    PORTA = 0xff;
+    PORTC = 0xff;
     // port B is input for Data Bus
     DDRB = 0x00;
+    // Enable pull-ups
+    PORTB = 0xff;
     // pin D4 is clock output
     DDRD |= _BV(DDD4);
     // pin D5 is R/W input
     DDRD &= ~_BV(DDD5);
+    // enable pull-up on D5
+    PORTD |= _BV(PORTD5);
 
     // start by pulling clock line low
     PORTD &= ~_BV(PORTD4);
@@ -65,10 +72,10 @@ int main(void)
         // raise clock line high
         PORTD |= _BV(PORTD4);
         // read address and data busses as well as r/W flag
-        currentAddrLSB = PORTA;
-        currentAddrMSB = PORTC;
-        currentData = PORTB;
-        currentReadWrite = PORTD & _BV(PORTD5);
+        currentAddrLSB = PINA;
+        currentAddrMSB = PINC;
+        currentData = PINB;
+        currentReadWrite = PIND & _BV(PIND5);
         // pull clock line low
         PORTD &= ~_BV(PORTD4);
 
@@ -83,6 +90,9 @@ int main(void)
         if (bufferIndex == BUFFER_SIZE)
         {
             bufferIndex = 0;
+            // for (int i=0; i<BUFFER_SIZE; i++) {
+            //     printf("%02X%02X %c %02X\n", addrMSB[i], addrLSB[i], readWrite[i] ? 'r' : 'W', data[i]);
+            // }
         }
     }
 
