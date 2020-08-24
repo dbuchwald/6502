ceb70b9359b652ef02758f47f689765aac6dcf8b
Dawid Buchwald <dawid.buchwald@hotmail.com>
2020-08-24 23:13:17 +0200

    Page write mode seems to be working just fine as well

--- AVR/programmer-atmega644/shell.c
+++ AVR/programmer-atmega644/shell.c
@@ -5,6 +5,7 @@
 void displayHelp(void);
 void dumpEEPROM(void);
 void writeEEPROM(void);
+void writeEEPROMPage(void);
 
 void runShell(void) {
   unsigned char keep_going=1;
@@ -29,6 +30,10 @@ void runShell(void) {
       case 'W':
         writeEEPROM();
         break;
+      case 'p':
+      case 'P':
+        writeEEPROMPage();
+        break;
       default:
         printf("ERROR: Unrecognized command %c [%02x], type 'h' for help...\n", c, c);
     }
@@ -39,6 +44,7 @@ void displayHelp(void) {
   printf("Please select one of the options:\n");
   printf(" [d]ump - dump EEPROM contents\n");
   printf(" [w]rite - write data to EEPROM\n");
+  printf(" [p]age write - write page of data\n");
   printf(" [h]elp - display this information\n");
   printf(" [q]uit - leave shell\n");
 }
@@ -94,5 +100,43 @@ void writeEEPROM(void) {
   }
   new_data = readSingleByte(address);
   printf(" - Data read: %02x\n", new_data);
+  returnBusControl();
+}
+
+void writeEEPROMPage(void) {
+  uint16_t address=0x8000;
+  uint8_t old_data, result;
+  uint8_t new_data[64];
+  printf("Writing page data to EEPROM\n");
+  assumeBusControl();
+  printf(" - Reading data at location %04x\n", address);
+  old_data = readSingleByte(address);
+  printf(" - Data read: %02x\n", old_data);
+  for (uint8_t i=0; i<64; i++) {
+    new_data[i] = ~old_data;
+  }
+  printf(" - Disabling write protection\n");
+  disableDataProtection();
+  printf(" - Writing %02x at location %04x\n", new_data[0], address);
+  result = writePage(address, new_data);
+  if (result) {
+    printf(" - Data written successfully\n");
+  } else {
+    printf(" - Write has failed\n");
+  }
+  printf(" - Enabling write protection\n");
+  enableDataProtection();
+  printf(" - Reading data again\n");
+  new_data[0] = readSingleByte(address);
+  printf(" - Data read: %02x\n", new_data[0]);
+  printf(" - Testing data protection\n");
+  result = writeSingleByte(address, ~new_data[0]);
+  if (result) {
+    printf(" - Data protection seems to have failed\n");
+  } else {
+    printf(" - Data protection worked correctly\n");
+  }
+  new_data[0] = readSingleByte(address);
+  printf(" - Data read: %02x\n", new_data[0]);
   returnBusControl();
 }
