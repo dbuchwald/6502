65ef74ee19282f6c6e79f3ef92e97105201337b4
Dawid Buchwald <dawid.buchwald@cgi.com>
2020-09-04 21:05:21 +0200

    Finally figured out timers in AVR...

--- AVR/programmer-atmega644/main.h
+++ AVR/programmer-atmega644/main.h
@@ -1,5 +1,7 @@
 int main(void);
 void initSystem(void);
+void initTimer(void);
 void assumeBusControl(void);
 void returnBusControl(void);
 void updateControlRegister(void);
+uint32_t getSystemTime(void);
