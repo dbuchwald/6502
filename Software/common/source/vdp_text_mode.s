1a8637899662e07baf76f5bd8e0695e3a330f10b
Kyle <kyle@e2eanalytics.com>
2021-08-16 10:25:38 -0700

    relocate text mode utility functions.

--- Software/common/source/vdp_text_mode.s
+++ Software/common/source/vdp_text_mode.s
@@ -7,9 +7,25 @@
     .include "zeropage.inc"
     .include "blink.inc"
 
-    .export scroll_line
+    .export vdp_text_init
+    .export vdp_scroll_line
     .export vdp_line
     .export vdp_char_pos
+
+    .export vdp_advance_char_position
+    .export vdp_newline
+    .export vdp_clear_line
+
+    .export vdp_backspace
+    .export vdp_set_prompt
+
+    .export vdp_out_char
+    .export vdp_write_char
+    .export vdp_write_string
+
+PROMPT = '>'
+SPACE = ' '
+
 
 
       .segment "BSS"
@@ -18,13 +34,255 @@ vdp_char_pos:          .byte $00
 
 
     .code
+
+;------------------------------------------------------------------------------
+;
+; VDPReset
+; Set Text Mode / Typical Settings
+;
+;------------------------------------------------------------------------------
+vdp_text_init:
+  jsr vdp_init_text_mode
+  jsr vdp_initialize_text_pattern_table
+  jsr vdp_clear_screen
+  jsr vdp_enable_display
+  rts
+
+
+;------------------------------------------------------------------------------
+;
+; AdvanceChar
+; Move to the next character position
+;
+;------------------------------------------------------------------------------
+vdp_advance_char_position:
+      .scope
+      pha
+
+      lda vdp_char_pos
+      cmp #VDP_TEXT_MODE_LINE_LENGTH - 1
+
+      beq do_not_advance_char
+
+      inc vdp_char_pos
+do_not_advance_char:      
+      pla
+      rts
+      .endscope
+
+;------------------------------------------------------------------------------
+;
+; AdvanceLine
+; Move to the next line on the screen; does NOT currenly change char pos
+;
+;------------------------------------------------------------------------------
+vdp_newline:
+      .scope
+      pha
+
+      lda vdp_line
+      cmp #VDP_TEXT_MODE_LINE_COUNT-1
+      beq scroll_required
+
+      inc
+      sta vdp_line
+      bra done
+
+scroll_required:
+      jsr vdp_scroll_line
+      jsr vdp_clear_line
+
+done:
+      pla
+      rts
+      .endscope      
+
+
+
+
+
+;------------------------------------------------------------------------------
+;
+; VDPWriteChar
+; Write the charater in acc to the active position on the screen and advance
+;
+;------------------------------------------------------------------------------
+vdp_write_char:
+      jsr load_vram_char_position
+      sta VDP_VRAM
+
+      jsr vdp_advance_char_position  
+      rts
+
+;------------------------------------------------------------------------------
+;
+; VDPOutChar
+; Write the charater in acc to the active position on the screen
+;
+;------------------------------------------------------------------------------
+vdp_out_char:
+      jsr load_vram_char_position
+      sta VDP_VRAM
+      rts      
+
+;------------------------------------------------------------------------------
+;
+; VDPWriteString
+; Write null terminated string at ADDR LSB = Acc, MSB = X and advance
+; Currently limited to 256 char string; use vdp_vram_write_buffer for long blocks
+;
+;------------------------------------------------------------------------------
+vdp_write_string:
+    .scope
+    sta vdp_buffer_address
+    stx vdp_buffer_address +1
+
+    jsr load_vram_char_position
+
+    phy
+    ldy #0
+write_loop:
+    lda (vdp_buffer_address),Y
+    beq write_end
+    sta VDP_VRAM
+    iny
+    bra write_loop  
+
+write_end:
+    jsr vdp_advance_char_position  
+    rts
+    .endscope
+
+
+
+;------------------------------------------------------------------------------
+;
+; LoadVRAMCharPosition
+; Loads the VDP VRAM address that corresponds to (vdp_line, vdp_char_pos)
+;
+;------------------------------------------------------------------------------
+load_vram_char_position:
+      .scope
+      pha
+      phx
+
+      lda #0
+      sta vdp_vram_address+1
+
+      lda vdp_char_pos                    ; keep LSB of VRAM Address here
+      ldx vdp_line
+      beq done
+
+next_line:
+      clc
+      adc #VDP_TEXT_MODE_LINE_LENGTH
+      bcc no_carry     
+      inc vdp_vram_address+1
+
+no_carry:      
+      dex
+      bne next_line
+
+done:
+      sta VDP_REG
+  
+      lda vdp_vram_address+1
+      ora #VDP_WRITE_VRAM_SELECT      
+      sta VDP_REG
+
+      plx
+      pla
+      rts
+      .endscope
+
+
+
+;------------------------------------------------------------------------------
+;
+; VDPClearLine
+; Clear current line - does not change the character position
+;
+;------------------------------------------------------------------------------
+vdp_clear_line:
+      .scope
+      pha
+      phx
+
+      ldx #0
+      stx vdp_char_pos
+
+      lda #' '
+      jsr load_vram_char_position
+
+loop:
+      sta VDP_VRAM
+      inx
+      cpx #VDP_TEXT_MODE_LINE_LENGTH
+      bne loop      
+ 
+      plx  
+      pla
+      rts
+      .endscope
+
+
+;------------------------------------------------------------------------------
+;
+; DoBackspace
+; Back up to the last character, replace it with a space, and stay there
+;
+;------------------------------------------------------------------------------
+vdp_backspace:
+      .scope
+      ldx vdp_char_pos
+      beq dont_backspace
+      dex 
+      beq dont_backspace
+
+      dec vdp_char_pos
+      lda #SPACE
+      jsr vdp_out_char
+;      jsr load_vram_char_position
+;      sta VDP_VRAM
+
+dont_backspace:
+      rts
+      .endscope
+
+
+;------------------------------------------------------------------------------
+;
+; SetupPrompt
+; Move to beginning of line; output a prompt, and advance
+;
+;------------------------------------------------------------------------------
+vdp_set_prompt:
+      .scope
+      pha
+
+      lda #0
+      sta vdp_char_pos
+
+      lda #PROMPT
+      jsr vdp_out_char      
+;      jsr load_vram_char_position
+;      sta VDP_VRAM
+
+      jsr vdp_advance_char_position
+
+      pla
+      rts
+      .endscope
+
+
+
 ;------------------------------------------------------------------------------
 ;
 ; ScrollLine
 ; Shift Text on Screen Up one line; does not alter current char position
 ;
 ;------------------------------------------------------------------------------
-scroll_line:
+vdp_scroll_line:
     .scope
     pha
 
